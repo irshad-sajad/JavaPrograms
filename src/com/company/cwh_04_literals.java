f0e2a046147df6d7b1061a79d248ef8109ae313a
Irshad Sajad <bhattjunaid187@gmail.com>
2024-02-01 19:16:54 +0530

    git commit -m "first commit"

---
+++ src/com/company/cwh_04_literals.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class cwh_04_literals {
+    public static void main(String[] args) {
+
+        byte age=22;
+        int age2=33;
+        short age3 = 25;
+        long l1 = 56666666666666L; //by default int i.e., why we put 'L' at last.
+        char ch='A';
+        float f1=5.6f; //by default double i.e., why we put 'f' at last.
+        double d1=4.55;
+
+        boolean a = true;
+        System.out.print(age);
+        String str="harry";
+        System.out.println(str);
+    }
+}
